fe406572c81ece0ebc8889ce711740cebbec2d34
prototypa <prototypearea.dev@gmail.com>
2022-12-01 19:16:36 -0500

    Add Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+			colors: {
+				primary: colors.purple,
+				secondary: colors.sky,
+			},
+			fontFamily: {
+				sans: ["'InterVariable'", ...defaultTheme.fontFamily.sans],
+			},
+		},
+  },
+  plugins: [],
+};
