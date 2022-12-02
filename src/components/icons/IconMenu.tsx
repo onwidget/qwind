8996b366a4ca38a327f9584466465df65c202f0d
prototypa <prototypearea.dev@gmail.com>
2022-12-01 22:26:39 -0500

    Add & update icons

--- src/components/icons/IconMenu.tsx
+++ src/components/icons/IconMenu.tsx
@@ -2,12 +2,12 @@ interface ItemProps {
   class?: string;
 }
 
-export const IconSun = (props: ItemProps) => {
+export const IconMenu = (props: ItemProps) => {
   const { class: className } = props;
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      class={`icon icon-tabler icon-tabler-sun ${className || "w-5 h-5"}`}
+      class={`icon icon-tabler icon-tabler-menu ${className || "w-5 h-5"}`}
       width="24"
       height="24"
       viewBox="0 0 24 24"
@@ -18,8 +18,8 @@ export const IconSun = (props: ItemProps) => {
       stroke-linejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <circle cx="12" cy="12" r="4"></circle>
-      <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"></path>
+      <line x1="4" y1="8" x2="20" y2="8"></line>
+      <line x1="4" y1="16" x2="20" y2="16"></line>
     </svg>
   );
 };
