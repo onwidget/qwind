5ddc3e8e7509449649fb3a27ef8bbd126dcaff49
prototypa <prototypearea.dev@gmail.com>
2022-12-01 21:00:52 -0500

    Fix minimal details

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import CallToAction from "~/components/widgets/CallToAction";
-import FAQs from "~/components/widgets/FAQs";
-import Features from "~/components/widgets/Features";
+
 import Hero from "~/components/widgets/Hero";
+import Features from "~/components/widgets/Features";
+import FAQs from "~/components/widgets/FAQs";
 import Stats from "~/components/widgets/Stats";
+import CallToAction from "~/components/widgets/CallToAction";
 
 export default component$(() => {
   return (
