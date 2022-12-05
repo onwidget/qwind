bdb4a8a8be917dca0100226f4c31d57484195124
prototypa <prototypearea.dev@gmail.com>
2022-12-05 00:00:52 -0500

    Fix minimal details

--- src/root.tsx
+++ src/root.tsx
@@ -22,7 +22,7 @@ export default component$(() => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/manifest.json" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
         <RouterHead />
         <DarkThemeLauncher />
       </head>
