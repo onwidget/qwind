a3f3eea8a1eb2a8265e8ce216751ee2d286728f9
prototypa <prototypearea.dev@gmail.com>
2023-06-06 23:21:20 -0400

    Add keys to loop

--- src/components/widgets/Header.tsx
+++ src/components/widgets/Header.tsx
@@ -45,8 +45,8 @@ export default component$(() => {
           >
             {menu && menu.items ? (
               <ul class="flex flex-col pt-8 md:pt-0 md:flex-row md:self-center w-full md:w-auto text-xl md:text-lg">
-                {menu.items?.map((item) => (
-                  <li class={`${item.items ? "dropdown" : ""}`}>
+                {menu.items?.map((item, key) => (
+                  <li key={key} class={`${item.items ? "dropdown" : ""}`}>
                     {item.href ? (
                       <a
                         href={item.href}
@@ -62,8 +62,8 @@ export default component$(() => {
 
                     {item.items ? (
                       <ul class="dropdown-menu rounded md:absolute pl-4 md:pl-0 md:hidden font-medium md:bg-white md:min-w-[200px] dark:md:bg-slate-800 drop-shadow-xl">
-                        {item.items.map((item2) => (
-                          <li>
+                        {item.items.map((item2, key2) => (
+                          <li key={key2}>
                             <a
                               class="font-medium rounded-t md:hover:bg-gray-100 dark:hover:bg-gray-700 py-2 px-4 block whitespace-no-wrap"
                               href={item2.href}
