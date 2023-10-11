f3eae1f22c06ec2a5b0781d325fa382976c7f78d
prototypa <prototypearea.dev@gmail.com>
2023-10-11 10:50:00 -0400

    Add tabler icon IconChevronDown.tsx

---
+++ src/components/icons/IconChevronDown.tsx
@@ -0,0 +1,24 @@
+interface ItemProps {
+  class?: string;
+}
+
+export default (props: ItemProps) => {
+  const { class: className } = props;
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class={`icon icon-tabler ${className || "w-5 h-5"}`}
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      stroke-width="2"
+      stroke="currentColor"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M6 9l6 6l6 -6"></path>
+    </svg>
+  );
+};
